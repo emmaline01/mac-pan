40acb3d06a6dc0d9b5d92177c71056ee48f546f0
emmaline01 <emmaline.mai@gmail.com>
2018-05-08 16:13:20 -0700

    Update Pan.java
    inserts pan image

--- Pan.java
+++ Pan.java
@@ -18,80 +18,82 @@ public class Pan
     public void move() {
         if (Window.key.pressed( "right" )) {
             if (canMove("right")) {
-                x = x + 10;
+                x = x + 7;
                 direction = "right";
             }
         }
         else if (Window.key.pressed( "left" )) {
             if (canMove("left")) {
-                x = x - 10;
+                x = x - 7;
                 direction = "left";
             }
         }
         else if (Window.key.pressed( "up" )) {
             if (canMove("up")) {
-                y = y - 10;
+                y = y - 7;
                 direction = "up";
             }
         }
         else if (Window.key.pressed( "down" )) {
             if (canMove("down")) {
-                y = y + 10;
+                y = y + 7;
                 direction = "down";
             }
         }
         else {
             if (direction.equals("right")) {
                 if (canMove("right")) {
-                    x = x + 10;
+                    x = x + 7;
                 }
             }
             else if (direction.equals("left")) {
                 if (canMove("left")) {
-                    x = x - 10;
+                    x = x - 7;
                 }
             }
             else if (direction.equals("up")) {
                 if (canMove("up")) {
-                    y = y - 10;
+                    y = y - 7;
                 }
             }
             else if (direction.equals("down")) {
                 if (canMove("down")) {
-                    y = y + 10;
+                    y = y + 7;
                 }
             }
         }
         
-        //Window.out.image( "panImage.png", x, y );
-        Window.out.color( "gray" );
-        Window.out.circle( x, y, 12 ); //radius = 12 arbitrarily
+        //image is 35x35
+        // x-17 and 7-17 bc otherwise coordinates are top left corner of pic, so this centers it
+        Window.out.image( "panImage.png", x-17, y-17 );
+//        Window.out.color( "gray" );
+//        Window.out.circle( x, y, 12 ); //radius = 12 arbitrarily
     }
     
     private boolean canMove(String direction) { 
-        if (x + 10 < 1000 - 12 && direction.equals( "right" )) {
-            //1000 - 5 for the size of the screen - radius of ball
-            if (maze[((y - 12)/25)][((x - 12 + 10 + 12)/25)] != 1) { 
+        if (x + 7 < 1000 - 12 && direction.equals( "right" )) {
+            //1000 - 7 for the size of the screen - radius of ball
+            if (maze[((y - 12)/25)][((x - 12 + 7 + 12)/25)] != 1) { 
                 //+12 to account for offset of array maze to graphic maze
-                //+10 to check for the movement right
+                //+7 to check for the movement right
                 //+12 to account for radius
                 return true;
             }
         }
-        else if (x - 10 > 12 && direction.equals( "left" )) {
-            if (maze[((y - 12)/25) ][((x - 12 - 10 + 6)/25)] != 1) {
+        else if (x - 7 > 12 && direction.equals( "left" )) {
+            if (maze[((y - 12)/25) ][((x - 12 - 7 + 6)/25)] != 1) {
                 //why tf it's 6 i have no clue.. radius = 12???
                 return true;
             }
         }
-        else if (y - 10 > 12 && direction.equals( "up" )) {
-            if (maze[((y - 12 - 10)/25)][((x - 12  + 6)/25)] != 1) {
+        else if (y - 7 > 12 && direction.equals( "up" )) {
+            if (maze[((y - 12 - 7)/25)][((x - 12  + 6)/25)] != 1) {
                 return true;
             }
         }
-        else if (y + 10 < 600 - 12 && direction.equals( "down" )) {
+        else if (y + 7 < 600 - 12 && direction.equals( "down" )) {
             //600 - 12 for the size of the screen - radius of ball
-            if (maze[((y - 12 + 10 + 12)/25)][((x - 12 + 6)/25)] != 1) {
+            if (maze[((y - 12 + 7 + 12)/25)][((x - 12 + 6)/25)] != 1) {
                 return true;
             }
         }
@@ -107,14 +109,14 @@ public class Pan
     }
     
     public boolean touchingMacaroni(int arrayY, int arrayX) {
-        //know that Macaroni radius = 5, pan radius = 12
+        //know that Macaroni radius = 7, pan radius = 12
 //        if (Math.abs( m.getX() - x) <= 17 || Math.abs( m.getY() - y) <= 17) {
 //            return true;
 //        }
 //        return false;
         
         if (maze[arrayY][arrayX] == 2 &&
-                        (Math.abs( (12+arrayX*25) - x) <= 15 && Math.abs( (12+arrayY*25) - y) <= 10)) {
+                        (Math.abs( (12+arrayX*25) - x) <= 15 && Math.abs( (12+arrayY*25) - y) <= 15)) {
             maze[arrayY][arrayX] = 0;
             return true;
         }
