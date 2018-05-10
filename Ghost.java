346406e8c354ff3c50707a229499d518e50204b0
sliu892 <32806593+sliu892@users.noreply.github.com>
2018-05-10 12:23:48 -0700

    Update Ghost.java

--- Ghost.java
+++ Ghost.java
@@ -22,14 +22,35 @@ public abstract class Ghost
         images = new TreeMap<String, String>();
     }
     
+    /**
+     * Put the images of individual ghosts when heading in different directions
+     */
     public abstract void setImages();
     
+    /**
+     * Find x-coordinate the image should move to
+     * @return target x-coordinate
+     */
     public abstract int targetX();
     
+    /**
+     * Find y-coordinate the image should move to
+     * @return target y-coordinate
+     */
     public abstract int targetY();
     
+    /**
+     * Move to specified coordinates.
+     * @param targetX target x-coordinate
+     * @param targetY target y-coordinate
+     */
     public abstract void moveTo(int targetX, int targetY);
     
+    /**
+     * Identifies whether or not the ghost can move forward in the direction it's facing
+     * @param direction The direction the ghost is heading in
+     * @return true if the ghost can continue moving
+     */
     public boolean canMove(String direction)
     {
         //TODO
@@ -64,21 +85,36 @@ public abstract class Ghost
         return false;
     }
     
+    /**
+     * Returns the x-coordinate of the ghost
+     * @return current x-coordinate of the ghost
+     */
     public int getX()
     {
         return x;
     }
     
+    /**
+     * Returns the y-coordinate of the ghost
+     * @return current y-coordinate of the ghost
+     */
     public int getY()
     {
         return y;
     }
     
+    /**
+     * Identifies whether or not the ghost is eaten by the pan.
+     * @return true if the ghost is touching the pan.
+     */
     public boolean eaten()
     {
         return pan.touchingGhost(this);
     }
     
+    /**
+     * The ghosts' movements when the pan eats the blue macaroni
+     */
     public void frightenedMove()
     {
         //TODO
@@ -105,4 +141,6 @@ public abstract class Ghost
             x +=10;
         }
     }
+    
+    
 }
