4a202bd04ca97b2e9af9fea5ff99610d870f46b8
anubha1 <38767075+anubha1@users.noreply.github.com>
2018-05-11 16:32:41 -0700

    Update Pinky.java
    Took out fields because Ghost's fields are used. Made some other changes to constructor. Should at least not crash when run.

--- Pinky.java
+++ Pinky.java
@@ -1,24 +1,15 @@
 import java.util.TreeMap;
 
+import apcs.Window;
+
 
 public class Pinky extends Ghost
 {
 
-    private int x, y, length, width;
-
-    private TreeMap<String, String> images;
-
-    private int[][] maze;
-
-    private Pan pan;
-
-    private String direction;
-
-
     public Pinky( int[][] m, Pan p )
     {
         super( m, p );
-        direction = "";
+        setImages();
     }
 
 
@@ -38,24 +29,31 @@ public class Pinky extends Ghost
     public int targetX()
     {
         int panX = pan.getX();
-        if ( pan.getDirection().equals( "up" ) || pan.getDirection().equals( "down" ))
+        if ( pan.getDirection().equals( "up" ) || pan.getDirection().equals( "down" ) )
         {
             return panX;
         }
-        else if ( pan.getDirection().equals( "left" ))
+        else if ( pan.getDirection().equals( "left" ) )
         {
             if ( panX - 20 >= 20 )
             {
                 return panX - 20;
             }
-            else 
+            else
             {
                 return panX;
             }
         }
-        else if ( pan.getDirection().equals( "right" ))
+        else if ( pan.getDirection().equals( "right" ) )
         {
-            if ( panX + 20 < 1000 )
+            if ( panX + 20 <= 800 - 20 )
+            {
+                return panX + 20;
+            }
+            else
+            {
+                return panX;
+            }
         }
         return 0;
     }
@@ -65,16 +63,98 @@ public class Pinky extends Ghost
     public int targetY()
     {
         int panY = pan.getY();
-
+        if ( pan.getDirection().equals( "left" ) || pan.getDirection().equals( "right" ) )
+        {
+            return panY;
+        }
+        else if ( pan.getDirection().equals( "up" ) )
+        {
+            if ( panY - 20 >= 20 )
+            {
+                return panY - 20;
+            }
+            else
+            {
+                return panY;
+            }
+        }
+        else if ( pan.getDirection().equals( "down" ) )
+        {
+            if ( panY + 20 <= 800 - 20 )
+            {
+                return panY + 20;
+            }
+            else
+            {
+                return panY;
+            }
+        }
         return 0;
     }
 
 
     @Override
-    public void moveTo( int targetX, int targetY )
+    public void move()
     {
-        // TODO Auto-generated method stub
+        int targetX = targetX();
+        int targetY = targetY();
 
+        // TODO
+        int diffX = Math.abs( targetX - x );
+        int diffY = Math.abs( targetY - y );
+        String xDir = "";
+        String yDir = "";
+
+        // Determines whether blinky should head right or left based on
+        // x-coordinate.
+        if ( targetX > x )
+        {
+            xDir = "right";
+        }
+        else if ( x > targetX )
+        {
+            xDir = "left";
+        }
+
+        // Determines whether blinky should head up or down based on
+        // y-coordinate.
+        if ( targetY > y )
+        {
+            yDir = "down";
+        }
+        else
+        {
+            yDir = "up";
+        }
+
+        if ( targetX > targetY && canMove( xDir ) )
+        {
+            direction = xDir;
+
+            if ( xDir.equals( "right" ) )
+            {
+                x = x + 5;
+            }
+            else
+            {
+                x = x - 5;
+            }
+        }
+        else if ( targetY > targetX && canMove( yDir ) )
+        {
+            direction = yDir;
+
+            if ( xDir.equals( "up" ) )
+            {
+                y = y - 5;
+            }
+            else
+            {
+                y = y + 5;
+            }
+        }
+
+        Window.out.image( images.get( direction ), x - 10, y - 10 );
     }
 
 }
