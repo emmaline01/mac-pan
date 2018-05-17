b4332155c47c0c0e026ed03ff8e2dbdb1b3552e4
anubha1 <38767075+anubha1@users.noreply.github.com>
2018-05-17 11:27:07 -0700

    Update Blinky.java

--- Blinky.java
+++ Blinky.java
@@ -1,21 +1,19 @@
-import java.util.ArrayList;
 import java.util.TreeMap;
+
 import apcs.Window;
+
 
 public class Blinky extends Ghost
 {
-    private int x, y, length, width;
-    private TreeMap<String, String> images;
-    private int[][] maze;
-    private Pan pan;
-    private String direction;
-    
+
     public Blinky( int[][] m, Pan p )
     {
         super( m, p );
         setImages();
     }
 
+
+    @Override
     public void setImages()
     {
         images.put( "up", "BlinkyUp.png" );
@@ -26,71 +24,19 @@ public class Blinky extends Ghost
         images.put( "revert", "RevertingBackGhost.png" );
     }
 
+
+    @Override
     public int targetX()
     {
         return pan.getX();
     }
 
+
+    @Override
     public int targetY()
     {
         return pan.getY();
     }
 
-    public void moveTo( int targetX, int targetY )
-    {
-        // TODO
-        int diffX = Math.abs( targetX - x );
-        int diffY = Math.abs( targetY - y );
-        String xDir = "";
-        String yDir = "";
-        
-        //Determines whether blinky should head right or left based on x-coordinate.
-        if (targetX > x)
-        {
-            xDir = "right";
-        }
-        else if (x > targetX)
-        {
-            xDir = "left";
-        }
-        
-        //Determines whether blinky should head up or down based on y-coordinate.
-        if (targetY > y)
-        {
-            yDir = "down";
-        }
-        else
-        {
-            yDir = "up";
-        }
-        
-        if (targetX > targetY && canMove(xDir))
-        {
-            direction = xDir;
-            
-            if (xDir.equals( "right" ))
-            {
-                x = x + 5;
-            }
-            else
-            {
-                x = x - 5;
-            }
-        }
-        else if (targetY > targetX && canMove(yDir))
-        {
-            direction = yDir;
-            
-            if (xDir.equals( "up" ))
-            {
-                y = y - 5;
-            }
-            else
-            {
-                y = y + 5;
-            }
-        }
-        
-        Window.out.image( images.get( direction ), x - 10, y - 10 );
-    }   
+
 }
