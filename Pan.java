0d604173a6f81340f7afe156b171a8c9ba3aab1d
anubha1 <38767075+anubha1@users.noreply.github.com>
2018-05-20 09:48:01 -0700

    Added Javadoc

--- Pan.java
+++ Pan.java
@@ -1,6 +1,19 @@
 import apcs.Window;
 
 
+/**
+ * The Pan class represents the player on the board. It is added to the board in
+ * the main method. It is controlled by the user, who navigates in through the
+ * maze as it eats macaroni while avoiding ghosts.
+ * 
+ *
+ * @author Emmaline Mai, Shannon Liu, Anubha Kale
+ * @version May 18, 2018
+ * @author Period: 5
+ * @author Assignment: MacPan
+ *
+ * @author Sources: None
+ */
 public class Pan
 {
     int x;
@@ -11,24 +24,26 @@ public class Pan
 
     String direction;
 
+
     /**
-     * Constructs a pan by setting the starting position and initializing the provate variables.
-     * @param m is the maze.
+     * Pan Constructor
+     * 
+     * @param m
+     *            2d int array that represents the maze
      */
     public Pan( int[][] m )
     {
-        x = 400; 
+        x = 400;
         y = 360;
         maze = m;
         direction = "";
-
     }
 
+
     /**
-     * 
-     * The move method allows the Pan to move when certain keys are pressed. 
-     * If checks if it can move in that direction, then moves by changing its coordinates, 
-     * redrawing itself, and changing the private variable that holds its direction.
+     * The Pan's move method moves the pan based on the user key input. It
+     * checks if the Pan can move in certain directions using the canMove method
+     * and adjusts its x and y fields accordingly.
      */
     public void move()
     {
@@ -96,30 +111,40 @@ public class Pan
             }
         }
 
-        Window.out.image( "pan1.png", x , y );
+        Window.out.image( "pan1.png", x, y );
     }
 
 
+    /**
+     * The canMove method indicates whether or not the Pan can move in the
+     * direction passed based on the coordinates of the board and maze.
+     * 
+     * @param direction
+     *            String containing the direction can be either "up", "right",
+     *            "down", or "left".
+     * @return true if the Pan can move in the direction specified, false
+     *         otherwise
+     */
     private boolean canMove( String direction )
     {
-        if ( direction.equals( "right" ) && ( x + 50 - 10 ) / 20 + 1<= maze[0].length
-                        && maze[( y - 10 ) / 20 + 1 ][( x + 25 ) / 20] != 1)
+        if ( direction.equals( "right" ) && ( x + 50 - 10 ) / 20 + 1 <= maze[0].length
+            && maze[( y - 10 ) / 20 + 1][( x + 25 ) / 20] != 1 )
         {
             return true;
         }
-        else if ( direction.equals( "left" ) && (x / 20) >= 0
-            && maze[(y - 10 ) / 20 + 1][(x - 5) / 20] != 1)
+        else if ( direction.equals( "left" ) && ( x / 20 ) >= 0
+            && maze[( y - 10 ) / 20 + 1][( x - 5 ) / 20] != 1 )
         {
             return true;
         }
         else if ( direction.equals( "up" ) && ( y - 10 ) / 20 + 1 >= 0
-            && maze[( y - 5 ) / 20 ][( x - 10 ) / 20 + 1 ] != 1)
+            && maze[( y - 5 ) / 20][( x - 10 ) / 20 + 1] != 1 )
         {
             return true;
         }
-        else if ( direction.equals( "down" ) && ( y + 50 -10) / 20 + 1 <= maze.length
-            && maze[ ( y + 25 - 10 ) / 20][ ( x - 10 ) / 20 + 1 ] != 1 
-            && maze[ ( y + 25 - 10 ) / 20][ ( x - 10 ) / 20 + 1 ] != 4 )
+        else if ( direction.equals( "down" ) && ( y + 50 - 10 ) / 20 + 1 <= maze.length
+            && maze[( y + 25 - 10 ) / 20][( x - 10 ) / 20 + 1] != 1
+            && maze[( y + 25 - 10 ) / 20][( x - 10 ) / 20 + 1] != 4 )
         {
             return true;
         }
@@ -127,6 +152,13 @@ public class Pan
     }
 
 
+    /**
+     * The touchingGhost method indicates whether the Pan is touching a Ghost.
+     * 
+     * @param g
+     *            the Ghost to be checked
+     * @return true if the Pan is touching the Ghost, false otherwise
+     */
     public boolean touchingGhost( Ghost g )
     {
         if ( Math.abs( g.getX() - x ) <= 10 && Math.abs( g.getY() - y ) <= 10 )
@@ -137,30 +169,44 @@ public class Pan
     }
 
 
+    /**
+     * The touchingMacaroni method takes the row and column of a maze and checks
+     * if the Pan is touching a Macaroni at that location.
+     * 
+     * @param arrayY
+     *            the column of the board
+     * @param arrayX
+     *            the row of the board
+     * @return true if Pan is touching a macaroni at that location, false
+     *         otherwise
+     */
     public boolean touchingMacaroni( int arrayY, int arrayX )
     {
-        // know that Macaroni radius = 7, pan radius = 12
-        // if (Math.abs( m.getX() - x) <= 17 || Math.abs( m.getY() - y) <= 17) {
-        // return true;
-        // }
-        // return false;
-
-        if ( maze[arrayY][arrayX] == 2 && 
-                        ( Math.abs( ( 10 + arrayX * 20 ) - (x + 10) ) <= 10 )
-                        && (Math.abs( (10 + arrayY * 20 ) - (y + 10) ) <= 10 ) )
+        if ( maze[arrayY][arrayX] == 2 && ( Math.abs( ( 10 + arrayX * 20 ) - ( x + 10 ) ) <= 10 )
+            && ( Math.abs( ( 10 + arrayY * 20 ) - ( y + 10 ) ) <= 10 ) )
         {
             maze[arrayY][arrayX] = 0;
             return true;
         }
         return false;
     }
-    
-    public boolean touchingBlueMacaroni(int arrayY, int arrayX)
+
+
+    /**
+     * The touchingMacaroni method takes the row and column of a maze and checks
+     * if the Pan is touching a Blue Macaroni at that location.
+     * 
+     * @param arrayY
+     *            the column of the board
+     * @param arrayX
+     *            the row of the board
+     * @return true if Pan is touching a Blue Macaroni at that location, false
+     *         otherwise
+     */
+    public boolean touchingBlueMacaroni( int arrayY, int arrayX )
     {
-        //TODO
-        if ( maze[arrayY][arrayX] == 5 && 
-                        ( Math.abs( ( 10 + arrayX * 20 ) - (x + 10) ) <= 10 )
-                        && (Math.abs( (10 + arrayY * 20 ) - (y + 10) ) <= 10 ) )
+        if ( maze[arrayY][arrayX] == 5 && ( Math.abs( ( 10 + arrayX * 20 ) - ( x + 10 ) ) <= 10 )
+            && ( Math.abs( ( 10 + arrayY * 20 ) - ( y + 10 ) ) <= 10 ) )
         {
             maze[arrayY][arrayX] = 0;
             return true;
@@ -168,18 +214,34 @@ public class Pan
         return false;
     }
 
+
+    /**
+     * Accessor method for Pan's x coordinate
+     * 
+     * @return x coordinate of Pan's current location
+     */
     public int getX()
     {
         return x;
     }
 
 
+    /**
+     * Accessor method for Pan's y coordinate
+     * 
+     * @return y coordinate of Pan's current location
+     */
     public int getY()
     {
         return y;
     }
 
 
+    /**
+     * Accessor method for Pan's x direction
+     * 
+     * @return direction The direction that the Pan is facing and moving in.
+     */
     public String getDirection()
     {
         return direction;
