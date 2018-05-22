bb47b5ef332299d4aa4c8746b3542d58b15c98eb
anubha1 <38767075+anubha1@users.noreply.github.com>
2018-05-22 12:02:03 -0700

    Create JUMacPanTest.java

---
+++ JUMacPanTest.java
@@ -0,0 +1,75 @@
+
+public class JUMacPanTest
+{
+
+    /*
+     * Blinky Tests
+     * 
+     * 
+     * 
+     */
+
+    /*
+     * BlueMacaroni Tests
+     * 
+     * 
+     * 
+     */
+
+    /*
+     * Counter Tests
+     * 
+     * 
+     * 
+     */
+
+    /*
+     * Ghost Tests
+     * 
+     * 
+     * 
+     */
+
+    /*
+     * Inky Tests
+     * 
+     * 
+     * 
+     */
+
+    /*
+     * Macaroni Tests
+     * 
+     * 
+     * 
+     */
+
+    /*
+     * Pan Tests
+     * 
+     * 
+     * 
+     */
+
+    /*
+     * Pinky Tests
+     * 
+     * 
+     * 
+     */
+
+    /*
+     * Sound Tests
+     * 
+     * 
+     * 
+     */
+
+    /*
+     * Timer Tests
+     * 
+     * 
+     * 
+     */
+
+}
