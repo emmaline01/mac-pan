31dffa81b75f71fdcd5a830821af4c7701876958
anubha1 <38767075+anubha1@users.noreply.github.com>
2018-05-26 14:58:25 -0700

    Added javadoc comments

--- Blinky.java
+++ Blinky.java
@@ -1,14 +1,43 @@
+/**
+ * The Blinky class represents a Blinky ghost in the game (the red ghost). Its
+ * purpose is to chase the Pan around the board. It extends the Ghost class and
+ * it is used in the main method of the MacPan class.
+ *
+ * @author Emmaline Mai, Shannon Liu, Anubha Kale
+ * @version May 26, 2018
+ * @author Period: 5
+ * @author Assignment: MacPan
+ *
+ * @author Sources:
+ *         http://www.todayifoundout.com/index.php/2015/10/ghosts-pac-man-work/
+ */
+
 public class Blinky extends Ghost
 {
 
+    /**
+     * Constructor
+     * 
+     * @param m
+     *            the maze represented by a 2D int array
+     * @param p
+     *            the Pan (or player) in the game
+     */
     public Blinky( int[][] m, Pan p )
     {
         super( m, p );
-        changeDir(3);
+        changeDir( 3 );
         setImages();
         timer.start( 5 );
     }
 
+
+    /**
+     * Blinky's implementation of Ghost's abstract method setImages. It sets the
+     * images of Blinky in the images tree map based on direction.
+     * 
+     * @see Ghost#setImages()
+     */
     public void setImages()
     {
         images.put( "up", "BlinkyUp.png" );
@@ -19,17 +48,30 @@ public class Blinky extends Ghost
         images.put( "revert", "RevertingBackGhost.png" );
     }
 
+
+    /**
+     * Blinky's implementation of Ghost's abstract method targetX. It finds a
+     * target location based on the location the Pan.
+     * 
+     * @return Blinky's target x location
+     * @see Ghost#targetX()
+     */
     public int targetX()
     {
         return pan.getX();
     }
 
 
-    @Override
+    /**
+     * Blinky's implementation of Ghost's abstract method targetY. It finds a
+     * target location based on the location the Pan.
+     * 
+     * @return Blinky's target y location
+     * @see Ghost#targetY()
+     */
     public int targetY()
     {
         return pan.getY();
     }
-
 
 }
